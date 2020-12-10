7fa91a2f5ac782d2004ef3e9087d3b83198abbdf
Karla Dayhoff <kddayhoff@gmail.com>
2020-12-10 08:46:08 -0600

    Reformatted code with prettier

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-$(document).ready(function(){
-    $("#currentDay").text(moment().format('MMMM Do YYYY'));
-});
-console.log(moment().format('MMMM Do YYYY'));
+var dailyItems = [];
 
- var dailyItems = [];
-function renderItems () {
-$(btn8).on("click", function(event) {
-  event.preventDefault();
+//Puts current day on the planner title
+$(document).ready(function () {
+  $("#currentDay").text(moment().format("MMMM Do YYYY"));
+});
+console.log(moment().format("MMMM Do YYYY"));
+
+function renderItems() {
+  $(btn8).on("click", function (event) {
+    event.preventDefault();
     var userText = $("#user-input8").val();
     var userInput = $("<h1>" + userText + "</h1>");
     $("#user-input8").append(userInput);
     console.log(userText);
     localStorage.setItem("8 AM", userText);
     dailyItems.push(userText);
-})
+  });
 
-$(btn9).on("click", function(event) {
+  $(btn9).on("click", function (event) {
     event.preventDefault();
     var userText = $("#user-input9").val();
     var userInput = $("<h1>" + userText + "</h1>");
@@ -23,9 +25,9 @@ $(btn9).on("click", function(event) {
     console.log(userText);
     localStorage.setItem("9 AM", userText);
     dailyItems.push(userText);
-})
+  });
 
-$(btn10).on("click", function(event) {
+  $(btn10).on("click", function (event) {
     event.preventDefault();
     var userText = $("#user-input10").val();
     var userInput = $("<h1>" + userText + "</h1>");
@@ -33,18 +35,16 @@ $(btn10).on("click", function(event) {
     console.log(userText);
     localStorage.setItem("10 AM", userText);
     dailyItems.push(userText);
-})
+  });
 }
 renderItems();
 
 function init() {
- 
   var storedItems = JSON.parse(localStorage.getItem("dailyItems"));
 
   if (storedItems !== null) {
     dailyItems = storedItems;
   }
-  
   renderItems();
 }
 
@@ -52,35 +52,29 @@ function storedItems() {
   // Stringify and set "todos" key in localStorage to todos array
   localStorage.setItem("dailyItems", JSON.stringify(dailyItems));
 }
-  var time = 11;
+var time = 11;
 
-  function timeColor() {
+function timeColor() {
+  for (var i = 0; i < time; i++);
 
-    for (var i = 0; i < time; i++);
-    
-    var past = $(".time-count").addClass(".past");
-    var present = $(".time-count").addClass(".present");
-    var future = $(".time-count").addClass(".future");
-    var currentTime = moment();
-    
-    if (time  < currentTime) { 
-      past.toggle();
-      past = "past";
-      console.log(past);
-    } 
-     else if (time = currentTime) {
-       present.toggle();
-       present = "present";
-       console.log(present);
-    } 
-     else (time > currentTime) 
-       future.toggle();
-       future = "future";
-      console.log(future);
-  } 
-  
+  var past = $(".time-count").addClass(".past");
+  var present = $(".time-count").addClass(".present");
+  var future = $(".time-count").addClass(".future");
+  var currentTime = moment();
+
+  if (time < currentTime) {
+    past.toggle();
+    past = "past";
+    console.log(past);
+  } else if ((time = currentTime)) {
+    present.toggle();
+    present = "present";
+    console.log(present);
+  } else time > currentTime;
+  future.toggle();
+  future = "future";
+  console.log(future);
+}
+
 timeColor();
-  $(".time-count").append(timeColor);
- 
-  
-
+$(".time-count").append(timeColor);
